df0912c0ed434616830d0f953f3e580bb9feb66d
Pranjali <pranjalik111@gmail.com>
2026-05-29 17:00:39 -0400

    Initial Commit

---
+++ TaskFlow/TaskFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowApp.swift
+//  TaskFlow
+//
+//  Created by Pranjali Kulkarni on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
